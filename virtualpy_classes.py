60334adf9bf07c1da2cb9b118f180cad43bb0d87
mhoff12358 <mhoff12358@gmail.com>
2015-03-10 21:28:02 -0500

    Added some unfinished classes for virutalpy.

--- virtualpy_classes.py
+++ virtualpy_classes.py
@@ -1,5 +1,29 @@
 
-class A():
-	def __init__(self):
-		self.a = 5
+class RawType(object):
+    def __init__(self, type_list):
+        self.type_list = type_list
 
+    def verify(self, vertex_element):
+        return True
+
+class VertexType(object):
+    def __init__(self, *args):
+        """
+        Each element in args is a type definition for the vertex
+        The type definition consists of 2 or 3 elements, the meta type
+        (position, tex coord...), the raw type (3 32 bit floats RGB...), and
+        optionally a name
+        """
+        self.type_def = args
+        self.total_num_elements = type_list
+
+    def getTypeDef(self):
+        return self.type_def
+
+    def createVertex(self, *args, **kwargs):
+        
+
+class Vertex(object):
+    def __init__(self, vertex_type, data_list):
+        self.vertex_type = vertex_type
+        self.data_list = data_list
